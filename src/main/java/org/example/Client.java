733e7abf5125fec2da690ee84ceca27f80c29554
Neto test <denis.157911@mail.ru>
2024-03-30 13:38:27 +0400

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        try (Socket clientSocket = new Socket("localhost", Main.PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Den");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+}
